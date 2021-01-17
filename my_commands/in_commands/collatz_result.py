92b78e702c5accdf2e3e99afa9bfe5c781e44cda
= <=>
2021-01-17 18:43:01 +0900

    change to reply from send

--- my_commands/in_commands/collatz_result.py
+++ my_commands/in_commands/collatz_result.py
@@ -4,13 +4,13 @@ import re
 async def celebrate(message):
     if message.channel.id != 740198572435308635 or message.author.id != 739586615487496274 or not re.search(r'^\(実行回数\d+回\)$', message.content):
         return
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
-    await message.channel.send('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
+    await message.reply('おめでとうございます')
