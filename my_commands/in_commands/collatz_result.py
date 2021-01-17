de74afa3027b7201b4af5bde13fcb122c0742966
= <=>
2021-01-17 18:38:52 +0900

    add function

---
+++ my_commands/in_commands/collatz_result.py
@@ -0,0 +1,16 @@
+#celebrate
+#'実行回数'
+import re
+async def celebrate(message):
+    if message.channel.id != 740198572435308635 or message.author.id != 739586615487496274 or not re.search(r'^\(実行回数\d+回\)$', message.content):
+        return
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
+    await message.channel.send('おめでとうございます')
