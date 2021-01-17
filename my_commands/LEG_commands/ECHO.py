8bb53e5bac102691a219dd6b15f1b7732b47008c
= <=>
2021-01-17 18:39:26 +0900

    change to reply from send

--- my_commands/LEG_commands/ECHO.py
+++ my_commands/LEG_commands/ECHO.py
@@ -2,4 +2,4 @@
 async def ECHO(m, message):
     if len(m) > 2:
         s = message.content[message.content.find(m[2], message.content.find(m[1])+5):]
-        await message.channel.send(s)
+        await message.reply(s)
