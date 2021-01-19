2e232a3d03dc0da298b251d9d675888e37900308
= <=>
2021-01-19 17:15:41 +0900

    add a function

---
+++ my_commands/LEG_commands/delete.py
@@ -0,0 +1,37 @@
+#delete
+a = b = False
+async def delete(m, message):
+    global a
+    global b
+    if not a:
+        if message.channel.id == 740198572435308635:
+            a = True
+            await message.channel.send('削除開始')
+            while True:
+                print('準備中')
+                messages = await message.channel.history(limit=3000, oldest_first=True).flatten()
+                print('逆順中')
+                messages.reverse()
+                i=0
+                for message in messages:
+                    try:
+                        await message.delete()
+                    except:
+                        print('error')
+                        pass
+                    print(i, 'test', message.content)
+                    i += 1
+    elif not b:
+        if message.channel.id == 740198572435308635:
+            b = True
+            await message.channel.send('削除開始')
+            while True:
+                print('準備中')
+                messages = await message.channel.history(limit=10000, oldest_first=False).flatten()
+                print('逆順中')
+                messages.reverse()
+                for message in messages:
+                    await message.delete()
+                    print('test', message.content)
+    else:
+        await message.channel.send('削除開始できませんでした')
